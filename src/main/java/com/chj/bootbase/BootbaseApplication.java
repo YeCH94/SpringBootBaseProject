3f5b5f53602b84102553b9eba426435f6ad660f8
ChanHoYe <32897307+YeCH94@users.noreply.github.com>
2018-12-31 16:30:35 +0900

    Update Application Class

--- src/main/java/com/chj/bootbase/BootbaseApplication.java
+++ src/main/java/com/chj/bootbase/BootbaseApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootexerciseApplication {
+public class BootbaseApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BootexerciseApplication.class, args);
+        SpringApplication.run(BootbaseApplication.class, args);
     }
 }
 
