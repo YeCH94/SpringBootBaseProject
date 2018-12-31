ebfe410eff6b03539d5371d90da44d372dc0f44d
ChanHoYe <32897307+YeCH94@users.noreply.github.com>
2018-12-31 14:21:06 +0900

    Initial commit

---
+++ src/main/java/com/ch/bootexercise/BootexerciseApplication.java
@@ -0,0 +1,14 @@
+package com.ch.bootexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootexerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootexerciseApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/ch/bootexercise/BootexerciseApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ch.bootexercise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootexerciseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
