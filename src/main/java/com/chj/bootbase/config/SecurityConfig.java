b2d1833b9e17845036b3178e8e981b70b9961e01
hjhng125 <33080926+hjhng125@users.noreply.github.com>
2018-12-31 15:30:46 +0900

    Commit Security login permission

--- src/main/java/com/chj/bootbase/config/SecurityConfig.java
+++ src/main/java/com/chj/bootbase/config/SecurityConfig.java
@@ -1,7 +1,28 @@
 package com.chj.bootbase.config;
 
+import com.chj.bootbase.service.CustomMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
+@EnableGlobalAuthentication
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomMemberService customMemberService;
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
 }
